e4ef315403e1eef0dd5adfe76625f9bbf579c93d
Antonio Sabino <aslimajr@outlook.com>
2022-07-28 11:37:40 -0300

    feat: criando as rotas de movies

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -10,4 +18,19 @@ type Movie struct {
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	route := mux.NewRouter()
+
+	route.HandleFunc("/movies", getMovies).Methods("GET")
+	route.HandleFunc("/movies/{id}", getMovieById).Methods("GET")
+	route.HandleFunc("/movies", createMovie).Methods("POST")
+	route.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	route.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", route))
 }
